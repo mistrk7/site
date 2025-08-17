15d7f5a7aa8e30aef3e8b4b4eccf1f2bdd88de76
misterk7_- <merezkhaan@gmail.com>
2025-08-17 22:02:56 +0100

    Init eleventy with base style

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+export default function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("./src/css/");
+
+    eleventyConfig.addPassthroughCopy("./src/fonts/everyday_ample.ttf");
+    eleventyConfig.addPassthroughCopy("./src/fonts/everyday_ample_black.ttf");
+    eleventyConfig.addPassthroughCopy("./src/fonts/everyday_ample_bold.ttf");
+
+
+    eleventyConfig.addWatchTarget("./src/css/");
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+}
