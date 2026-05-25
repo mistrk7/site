8deaa79b8a92a894505f5f8ebbf2f63f74469141
misterk7_- <66379077+mistrk7@users.noreply.github.com>
2026-05-25 21:41:56 +0100

    Update comment for rot13 decoder function

--- src/javascript/rot13.js
+++ src/javascript/rot13.js
@@ -1,4 +1,4 @@
-// rot13 decoder
+// decoder
 function decode(a) {
     return a.replace(/[a-zA-Z]/g, function (c) {
         return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
